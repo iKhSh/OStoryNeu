e6048863656b37d1883b763b3f19a9994a7dd995
Khaled <khalidshiba95@gmail.com>
2023-06-01 15:52:11 +0200

    Initial Commit

---
+++ OStory/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  OStory
+//
+//  Created by Khalid Shiba on 01.06.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OStory/OStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  OStoryApp.swift
+//  OStory
+//
+//  Created by Khalid Shiba on 01.06.23.
+//
+
+import SwiftUI
+
+@main
+struct OStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
